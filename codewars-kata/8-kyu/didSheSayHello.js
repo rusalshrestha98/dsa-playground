1590c66eb1f1b8606f744fdfdd0ac7a6c1ac6bb3
Rusal Shrestha <rusalo101@gmail.com>
2021-10-10 23:42:57 -0400

    work in progress kata

---
+++ codewars-kata/8-kyu/didSheSayHello.js
@@ -0,0 +1,21 @@
+// You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
+
+// Write a simple function to check if the string contains the word hallo in different languages.
+
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+
+const validateHello = (greeting) => {
+    let input = greeting.toLowerCase();
+    const allGreetings = ["hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"];
+    return allGreetings.includes(greeting);
+}
+
+console.log(validateHello("hello"));
