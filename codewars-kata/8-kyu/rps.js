1a746ab3a601d6eadf62013c620f20f9ed6c87c8
Rusal Shrestha <rusalo101@gmail.com>
2022-06-28 16:33:50 -0400

    completed kata

---
+++ codewars-kata/8-kyu/rps.js
@@ -0,0 +1,39 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+const rps = (p1, p2) => {
+  if (p1 === 'scissors' && p2 === 'paper') {
+    return "Player 1 won!"
+  } else if (p1 === "scissors" && p2 === 'rock') {
+    return "Player 2 won!" 
+  } else if (p1 === "rock" && p2 === 'paper') {
+    return "Player 2 won!"
+  } else if (p1 === "paper" && p2 === 'scissors') {
+    return "Player 2 won!"
+  } else if (p1 === "rock" && p2 === 'scissors') {
+    return "Player 1 won!"
+  } else if (p1 === "paper" && p2 === 'rock') {
+    return "Player 1 won!"
+  } else if (p1 === "scissors" && p2 === 'paper') {
+    return "Player 1 won!"
+  } else {
+    if (p1 === p2) {
+      return "Draw!"
+    }
+  }
+};
+
+// the better way to do it
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//   if (p2 === rules[p1]) {
+//     return "Player 1 won!";
+//   }
+//   else {
+//     return "Player 2 won!";
+//   }
+// };
+
+console.log(rps('rock', 'scissors'));
+console.log(rps('scissors', 'paper'));
+console.log(rps('paper', 'rock'));
