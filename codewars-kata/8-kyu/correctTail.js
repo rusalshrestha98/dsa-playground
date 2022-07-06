42f52ebc7192280b7bb13ffd02cc3094158abc12
Rusal Shrestha <rusalo101@gmail.com>
2022-07-05 22:36:00 -0400

    saving kata

---
+++ codewars-kata/8-kyu/correctTail.js
@@ -0,0 +1,17 @@
+// function correctTail(bod, tail ;
+  
+//   sub = body.substr(bodylength-(tail.length)
+  
+//   if sub = tail) ;
+//     return true
+//   }
+//   else 
+//     return false
+
+const correctTail = (bod, tail) => {
+  // sub = bod.substr(body.length-(tail.length));
+  console.log(tail.length);
+  // return sub;
+}
+
+console.log(correctTail("Fox", "x"));
