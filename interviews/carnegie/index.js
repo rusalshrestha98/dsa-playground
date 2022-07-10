dbc38e3bc4112afb808f0bd2edfd2aaccb302cfc
Rusal Shrestha <rusalo101@gmail.com>
2022-07-10 01:11:31 -0400

    onclick events for buttons

--- interviews/carnegie/index.js
+++ interviews/carnegie/index.js
@@ -1,3 +1,11 @@
 const showLightbox = () => {
   document.getElementById('lightbox').style.display = "block";
 }
+
+const registerNow = () => {
+  alert('Register Now button was clicked!');
+}
+
+const viewVisitOptions = () => {
+  alert('View Visit Options button was clicked!');
+}
