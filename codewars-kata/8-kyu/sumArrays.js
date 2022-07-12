aab8fbf1a6bdf5bbcb650d70e3d2428bc7e3bda1
Rusal Shrestha <rusalo101@gmail.com>
2022-07-12 14:25:16 -0400

    completed kata

--- codewars-kata/8-kyu/sumArrays.js
+++ codewars-kata/8-kyu/sumArrays.js
@@ -2,7 +2,12 @@
 // The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 
 const sum = (numbers) => {
-  return numbers;
+  let sum = 0;
+  numbers.map((x) => {
+    sum += x;
+  })
+  return sum;
 }
 
 console.log(sum([1,5.2,4,0,-1]));
+console.log(sum([]));
