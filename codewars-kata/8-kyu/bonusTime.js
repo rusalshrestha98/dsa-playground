d59eb31e9f9781753b76b5a2e5fcff64cb03ad1c
Rusal Shrestha <rusalo101@gmail.com>
2022-07-27 23:04:20 -0400

    completed kata

---
+++ codewars-kata/8-kyu/bonusTime.js
@@ -0,0 +1,22 @@
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... 
+// but who is going to make the most money?
+
+// Build a function that takes in two arguments (salary, bonus). Salary will 
+// be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, 
+// the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with 
+// "£" (= "\u00A3", JS, Go, Java and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, 
+// PHP, Python, Haskell and Lua) or "¥" (Rust).
+
+const bonusTime = (salary, bonus) => {
+  let a = bonus ? salary * 10 : salary;
+  return "£" + a.toString();
+}
+
+console.log(bonusTime(10000, true));
+console.log(bonusTime(25000, true));
+console.log(bonusTime(10000, false));
+console.log(bonusTime(2, true));
