ae47a76ab5f66187efe0c5418605028f4d9535e3
Rusal Shrestha <rusalo101@gmail.com>
2022-07-29 16:08:25 -0400

    kata in progress

---
+++ codewars-kata/8-kyu/countBy.js
@@ -0,0 +1,18 @@
+// Create a function with two arguments that will return an array 
+// of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count 
+// will be positive numbers greater than 0.
+
+// Return the results as an array
+
+const countBy = (x,n) => {
+  let result = [];
+  for (let i = x; i <= n; i++) {
+    let 
+  }
+  return result;
+}
+
+console.log(countBy(1,10));
+console.log(countBy(2,5));
