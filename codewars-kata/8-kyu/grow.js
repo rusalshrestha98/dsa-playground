319cb801574a7c073627869b79f6668161260810
Rusal Shrestha <rusalo101@gmail.com>
2022-08-10 14:42:09 -0400

    completed kata

--- codewars-kata/8-kyu/grow.js
+++ codewars-kata/8-kyu/grow.js
@@ -1,9 +1,13 @@
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-const grow = (x) => {
+const grow = (array) => {
+  return array.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue;
+  });
+};
 
-}
+// const grow=x=> x.reduce((a,b) => a*b);
 
 console.log(grow([1,2,3]));
 console.log(grow([4,1,1,1,4]));
