d5f00f93a5414570aa4e724f6fc5adcdbd0ddf68
Rusal Shrestha <rusalo101@gmail.com>
2022-08-15 18:56:37 -0400

    kata in progress

---
+++ codewars-kata/8-kyu/isUpperCase.js
@@ -0,0 +1,12 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// In this Kata, a string is said to be in ALL CAPS whenever it does not 
+// contain any lowercase letter so any string containing no letters at all 
+// is trivially considered to be in ALL CAPS.
+
+const isUpperCase = () => {
+
+}
+
+console.log(isUpperCase("C is upper case."));
+console.log(isUpperCase("c is not upper case."));
