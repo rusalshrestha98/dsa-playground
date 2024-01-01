37e433b094ffb967d897bd419dd9fbeb56969433
rusalshrestha98 <rusalo101@gmail.com>
2023-12-31 21:24:51 -0500

    completed kata

---
+++ codewars/8-kyu/points.js
@@ -0,0 +1,41 @@
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+const points = (games) => {
+  let totalPoints = 0;
+
+  games.map((game) => {
+    if (game[0] > game[2]) {
+      totalPoints += 3;
+    } else if (game[0] === game[2]) {
+      totalPoints += 1;
+    } else {
+      return;
+    }
+  });
+
+  return totalPoints;
+};
+
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
+);
+console.log(
+  points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"])
+);
