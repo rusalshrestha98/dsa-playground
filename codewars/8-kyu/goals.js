ac80b0fc7a45c5bdb15892cabd4dcbc2bd3b3b6e
rusalshrestha98 <rusalo101@gmail.com>
2024-01-01 18:09:15 -0500

    completed kata

---
+++ codewars/8-kyu/goals.js
@@ -0,0 +1,10 @@
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+const goals = (a, b, c) => a + b + c;
--- codewars/8-kyu/removeEveryOther.js
+++ codewars/8-kyu/removeEveryOther.js
@@ -2,15 +2,23 @@
 // Always keep the first element and start removing with the next element.
 // None of the arrays will be empty, so you don't have to worry about that!
 
+// const removeEveryOther = (arr) => {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//       result.push(arr[i]);
+//     };
+//   };
+//   return result;
+// }
+
 const removeEveryOther = (arr) => {
   let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 !== 0) {
-      result.push(arr[i]);
-    };
-  };
+  arr.map((el, i) => {
+    if (i % 2 === 0) result.push(el);
+  });
   return result;
-}
+};
 
 // console.log(removeEveryOther([1,2,3,4,5,6,7,8,9,10]));
 console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
