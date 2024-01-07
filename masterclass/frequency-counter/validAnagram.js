547c115e720bd4339b5cce758284afdca579eb06
rusalshrestha98 <rusalo101@gmail.com>
2024-01-07 16:38:33 -0500

    frequency counters

---
+++ masterclass/frequency-counter/validAnagram.js
@@ -0,0 +1,30 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first.
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+const validAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  // create the frequency counter using the first string
+  let frequencyCounter = {};
+  for (let val of s) {
+    frequencyCounter[val]
+      ? frequencyCounter[val]++
+      : (frequencyCounter[val] = 1);
+  }
+
+  // now loop over second string
+  for (let i = 0; i < t.length; i++) {
+    // compare each letter to the frequency counter
+    let letter = t[i];
+
+    // if the frequency counter has the letter, return false
+    if (!frequencyCounter[letter]) return false;
+    // if the frequency counter doesn't have the letter, subtract 1 from the letter frequency
+    else frequencyCounter[letter] -= 1;
+  }
+
+  return true;
+};
+
+console.log(validAnagram("anagram", "nagaram"));
+console.log(validAnagram("rat", "car"));
