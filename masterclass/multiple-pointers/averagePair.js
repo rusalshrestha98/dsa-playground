2b77e5894039b0259d8cc690e766b556069f4c98
rusalshrestha98 <rusalo101@gmail.com>
2024-01-11 08:30:10 -0500

    masterclass practice problem

--- masterclass/multiple-pointers/averagePair.js
+++ masterclass/multiple-pointers/averagePair.js
@@ -2,7 +2,20 @@
 // determine if there is a pair of values in the array where the average of the pair equals the target average.
 // There may be more than one pair that matches the average target.
 
-const averagePair = (arr, targetAverage) => {};
+const averagePair = (arr, targetAverage) => {
+  if (arr.length === 0) return false;
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let average = (arr[left] + arr[right]) / 2;
+    if (average === targetAverage) return true;
+    else if (average < targetAverage) left++;
+    else right--;
+  }
+  return false;
+};
 
 console.log(averagePair([1, 2, 3], 2.5));
 console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
