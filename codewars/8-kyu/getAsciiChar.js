b3d041988fb0e122b67e14cb3f44de5ebc7d1c05
rusalshrestha98 <rusalo101@gmail.com>
2024-01-22 13:27:45 -0500

    completed kata

---
+++ codewars/8-kyu/getAsciiChar.js
@@ -0,0 +1,8 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+const getChar = (c) => {
+  return String.fromCharCode(c);
+};
+
+console.log(getChar(55));
+console.log(getChar(56));
