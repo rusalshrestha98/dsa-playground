2c6d4a4968ca0fb7e012dd415edfc64723b981d5
rusalshrestha98 <rusalo101@gmail.com>
2024-01-31 20:07:56 -0500

    incomplete kata

---
+++ codewars/8-kyu/pipeFix.js
@@ -0,0 +1,16 @@
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values
+// increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+const pipeFix = (numbers) => {
+  let result = [];
+  let start = numbers[0];
+  let end = numbers[numbers.length - 1];
+
+  while (start < end) {
+    result.push[start];
+    start++;
+  }
+  return result;
+};
+
+console.log(pipeFix([1, 3, 5, 6, 7, 8]));
