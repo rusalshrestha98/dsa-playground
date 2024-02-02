09bd73e865d5aa09579b8315f24be7fb817e4136
rusalshrestha98 <rusalo101@gmail.com>
2024-02-01 21:32:17 -0500

    completed kata

--- codewars/8-kyu/pipeFix.js
+++ codewars/8-kyu/pipeFix.js
@@ -6,10 +6,10 @@ const pipeFix = (numbers) => {
   let start = numbers[0];
   let end = numbers[numbers.length - 1];
 
-  while (start < end) {
-    result.push[start];
-    start++;
+  for (let i = start; i <= end; i++) {
+    result.push(i);
   }
+
   return result;
 };
 
