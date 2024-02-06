beaa4390d037f1ad9c44368cb4aa99b964682595
rusalshrestha98 <rusalo101@gmail.com>
2024-02-05 19:05:40 -0500

    recursion practice problem

--- udemy/recursion/power.js
+++ udemy/recursion/power.js
@@ -5,7 +5,9 @@
 
 const power = (base, exp) => {
   if (exp === 0) return 1;
-  return base + power(base, exp - 1);
+  return base * power(base, exp - 1);
 };
 
+console.log(power(2, 0));
 console.log(power(2, 2));
+console.log(power(2, 4));
