c33a5ce52bbc3be6bd114895d5f1efbfdecbfebd
rusalshrestha98 <rusalo101@gmail.com>
2024-02-21 18:49:46 -0500

    completed katas

--- codewars/8-kyu/noBoringZeros.js
+++ codewars/8-kyu/noBoringZeros.js
@@ -2,3 +2,16 @@
 // They might be fun in your world, but not here.
 // Get rid of them. Only the ending ones.
 
+const noBoringZeros = (num) => {
+  let arr = num.toString().split('');
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === '0') {
+      arr.pop();
+    } else {
+      break;
+    }
+  }
+  return Number(arr.join(''));
+};
+
+console.log(noBoringZeros(1450));
