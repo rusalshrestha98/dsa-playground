37d500800109c5831d850200fef84348090800e2
rusalshrestha98 <rusalo101@gmail.com>
2025-01-23 19:08:37 -0500

    feat(codewars): completed 8 kyu kata"

---
+++ codewars/8-kyu/printArray.js
@@ -0,0 +1,8 @@
+// Input: Array of elements
+// Output: String with comma delimited elements of the array in th same order.
+
+const printArray = (arr) => {
+  return arr.join(',');
+};
+
+console.log(printArray(['h', 'o', 'l', 'a']));
