1ad41ca22f361b31a94b0bde00f30db08e4d12a2
rusalshrestha98 <rusalo101@gmail.com>
2026-01-25 18:03:18 -0500

    completed kata

---
+++ codewars/8-kyu/cookie.js
@@ -0,0 +1,24 @@
+// For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie.
+// If the input is a float or an int then "Monica" ate the cookie.
+// If the input is anything else "the dog" ate the cookie.
+// The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+// Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+// Note: Make sure you return the correct message with correct spaces and punctuation.
+
+// Please leave feedback for this kata. Cheers!
+
+const cookie = (x) => {
+  if (typeof x === 'string') {
+    name = "Zach"
+  } else if (typeof x === 'float' || typeof x === 'number') {
+    name = "Monica"
+  } else {
+    name = 'the dog'
+  }
+  return `Who ate the last cookie? It was ${name}!`;
+};
+
+
+console.log(cookie(26))
