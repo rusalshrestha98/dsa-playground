57d7f4c0d2a4ad63ae8c3d43c38f562ad62178fe
rusalshrestha98 <rusalo101@gmail.com>
2026-01-27 22:57:43 -0500

    completed kata

---
+++ codewars/8-kyu/ghost.js
@@ -0,0 +1,8 @@
+// Create a class Ghost
+// Ghost objects are instantiated without any arguments.
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+let Ghost = function() {
+  const colors = ["white", "yellow", "purple", "red"];
+  this.color = colors[Math.floor(Math.random() * colors.length)];
+};
